33b0d540c68c65231d29a578b4c95c106d6fc17c
Yaroslav <jarekpavlov@gmail.com>
2020-07-30 15:36:42 +0300

    Adding list of comments using thymeleaf and creating filter by tag

--- NewMessenger/src/main/java/com/messenger/repositories/MessageRepo.java
+++ NewMessenger/src/main/java/com/messenger/repositories/MessageRepo.java
@@ -1,9 +1,13 @@
 package com.messenger.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.messenger.domain.Message;
 
 public interface MessageRepo extends JpaRepository<Message, Long> {
+
+	List <Message> findByTag(String tag);
 
 }
--- NewMessenger/src/main/java/com/messenger/webcontrollers/ViewMessages.java
+++ NewMessenger/src/main/java/com/messenger/webcontrollers/ViewMessages.java
@@ -1,7 +1,10 @@
 package com.messenger.webcontrollers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,11 +16,13 @@ import com.messenger.repositories.MessageRepo;
 public class ViewMessages {
 	
 	@Autowired
-	MessageRepo message;
+	MessageRepo messageRepo;
 	
 	@GetMapping("/messages")
-	public String getMessages() {
+	public String getMessages(ModelMap model) {
 		
+		List<Message> listOfMessages = messageRepo.findAll();
+			model.put("messages",listOfMessages );
 		return "messages";
 	
 	}
@@ -28,11 +33,25 @@ public class ViewMessages {
 		Message newMessage = new Message();
 		newMessage.setText(text);
 		newMessage.setTag(tag);
+		messageRepo.save(newMessage);
+		return "messages";
 		
-		message.save(newMessage);
+	}
+	
+	@PostMapping("/filter")
+	public String filter(ModelMap model, @RequestParam(required = false) String filter) {
+		List<Message> list;
+		if(!filter.isEmpty()) {
+			list=messageRepo.findByTag(filter);
+			
+		}else {
+			list=messageRepo.findAll();
+			  }
+		model.put("messages", list);
 		
 		return "messages";
 		
 	}
+
 
 }
