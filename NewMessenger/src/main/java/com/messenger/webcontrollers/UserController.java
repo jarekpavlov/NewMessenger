6168b1943de39249d32d765799059af80db217eb
Yaroslav <jarekpavlov@gmail.com>
2020-08-09 00:08:45 +0300

    Creating page for editing user data

--- NewMessenger/src/main/java/com/messenger/webcontrollers/UserController.java
+++ NewMessenger/src/main/java/com/messenger/webcontrollers/UserController.java
@@ -1,11 +1,13 @@
 package com.messenger.webcontrollers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.messenger.domain.User;
 import com.messenger.repositories.UserRepo;
@@ -23,8 +25,22 @@ public class UserController {
 		List<User> allUsers =userRepo.findAll();
 		model.put("users", allUsers);		
 		
-		return "editingusers";
+		return "userinfo";
 	}
+	
+	@GetMapping("/users/{userId}")
+	public String changeUser(ModelMap model, @PathVariable Long userId) {
+		
+		Optional <User> userOpt=userRepo.findById(userId);
+		User user = new User();
+		if (userOpt.isPresent()) {
+			user = userOpt.get();
+			}
+		model.put("user", user);
+		return "useredit";
+	}
+	
+	
 	
 
 }
