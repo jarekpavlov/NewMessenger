1b819748aa94f991b59ee6e2a0b03cf3726360c9
Yaroslav <jarekpavlov@gmail.com>
2020-08-11 22:54:59 +0300

    Creating get request to map user data on editing page, comparing two strings in controller using string.equals()

--- NewMessenger/src/main/java/com/messenger/webcontrollers/UserController.java
+++ NewMessenger/src/main/java/com/messenger/webcontrollers/UserController.java
@@ -1,16 +1,20 @@
 package com.messenger.webcontrollers;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.messenger.domain.User;
 import com.messenger.repositories.UserRepo;
+import com.messenger.security.Authority;
 
 @Controller
 public class UserController {
@@ -30,12 +34,38 @@ public class UserController {
 	
 	@GetMapping("/users/{userId}")
 	public String changeUser(ModelMap model, @PathVariable Long userId) {
-		
+
 		Optional <User> userOpt=userRepo.findById(userId);
 		User user = new User();
 		if (userOpt.isPresent()) {
 			user = userOpt.get();
 			}
+
+		Set<Authority> authoritys = user.getAuthorities();
+		
+		String roleUser = "ROLE_USER";
+		String roleAdmin = "ROLE_ADMIN";
+		boolean hasUserRole= false;
+		boolean hasAdminRole = false;
+		
+		for (Authority authority:authoritys) {
+			String authorityStr = authority.getAuthority();
+			
+			if (authorityStr.equals(roleUser)) {
+				hasUserRole=true;
+			}
+			
+			if (authorityStr.equals(roleAdmin)) {
+				hasAdminRole=true;
+			}
+			
+		}
+		System.out.println(hasUserRole);
+		System.out.println(hasAdminRole);
+		
+
+		model.put("userRole", hasUserRole);
+		model.put("adminRole", hasAdminRole);
 		model.put("user", user);
 		return "useredit";
 	}
